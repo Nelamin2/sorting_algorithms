564d85040dd3aad74b6497ebb991c098280906ba
Nelamin2 <nidalelamin@gmail.com>
2024-01-23 14:21:07 -0800

    task_radix

---
+++ 105-radix_sort.c
@@ -0,0 +1,81 @@
+#include "sort.h"
+
+int find_max(int *array, int size);
+void radix_counting(int *array, size_t size, int sig, int *new);
+void radix_sort(int *array, size_t size);
+
+/**
+ * find_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int find_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * radix_counting - Sort the significant digits of an array of integers
+ * in ascending order using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @sig: The significant digit to sort on.
+ * @new: A buffer to store the sorted array.
+ */
+void radix_counting_sort(int *array, size_t size, int sig, int *new)
+{
+	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / sig) % 10] += 1;
+
+	for (i = 0; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		new[count[(array[i] / sig) % 10] - 1] = array[i];
+		count[(array[i] / sig) % 10] -= 1;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = buff[i];
+}
+
+/**
+ * radix_sort - Sort an array of integers in ascending
+ * order using the radix sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ **/
+void radix_sort(int *array, size_t size)
+{
+	int max, sig, *new;
+
+	if (array == NULL || size < 2)
+		return;
+
+	new = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	max = find_max(array, size);
+	for (sig = 1; max / sig > 0; sig *= 10)
+	{
+		radix_counting(array, size, sig, new);
+		print_array(array, size);
+	}
+
+	free(new);
+}
